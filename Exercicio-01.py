414b7f7b3fc917ed9de079057f2789b3f7dc051d
rodl11ndo <samuel.m.neto@ba.estudante.senai.br>
2023-11-23 20:21:31 -0300

    commit exercicios

---
+++ Exercicio-01.py
@@ -0,0 +1,38 @@
+#nome = (input("Qual o Seu Nome? "))
+#idade = int(input("Qual a Sua Idade? "))
+#altura = float(input("Qual a Sua Altura? "))
+#estudante = True
+
+#print(type(nome))
+#print(type(idade))
+#print(type(altura))
+#print(type(estudante))
+
+soma = 5 + 3
+diferenca = 5 - 3
+produto = 5 * 3
+divisao = 5 / 3
+
+print("\n")
+print("Os Resultados Matemáticos são:")
+print("Soma =",[soma], " Diferença =",[diferenca], " Produto =",[produto], " Divisão =",[divisao])
+
+igual = 5 == 3
+diferente = 5 !=3
+maior = 5 > 3
+menor = 5 < 3
+
+print("\n")
+print("Os Operadores de Comparação São:")
+print("Igual =",[igual], " Diferente =",[diferente], " Maior =",[maior], " Menor =",[menor])
+
+valor1 = True
+valor2 = False
+
+e = valor1 and valor2
+ou = valor1 or valor2
+nao = not True
+
+print("\n")
+print("Os Operadores Logicos São:")
+print("Operador AND:",[e],"Operador OR:",[ou],"Operador NOT:",[nao])
---
+++ Exercicio-02.py
@@ -0,0 +1,16 @@
+#idade = int(input("Digite sua Idade? "))
+idade = 17
+verdadeiro = True
+falso = False
+
+if idade >= 18:
+    print("Usuário é Maior de Idade: ", [idade])
+else:
+    print("Usuário Não é Maior de Idade: ", [idade])
+
+status = True
+
+if status:
+    print("Verdadeiro")
+else:
+    print("Falso")
