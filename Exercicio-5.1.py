d28e0f61aa7567bd08346703d86cce4989ec664c
rodl11ndo <samuel.m.neto@ba.estudante.senai.br>
2023-11-30 21:16:10 -0300

    commit exercicios

--- Exercicio-5.1.py
+++ Exercicio-5.1.py
@@ -4,35 +4,40 @@ usuarios = {
     'rod':'125'
 }
 
-print("Olá, seja bem vindo\n essas são as funções desse sistema;\npara criar um usuario digite: [0]\n para fazer login digite:[1]\n aperte 2 ou algum nomero inteiro maior para encerrar o sistema")
+print("Olá, seja bem vindo\nessas são as funções desse sistema;\npara criar um usuario digite: [0]\npara fazer login digite:[1]\npara encerrar o sistema aperte:[2]")
 opcao = input("qual função voce deseja acessar? ")
 
-while opcao < 3:
-    if opcao == 0:
-        criar_cadastro():
-
-    elif opcao == 1:
-            
-
-
-
+       
 
 def criar_cadastro():
-    user = str(input("informe o seu nome de usuario "))
+    user = input("informe o seu nome de usuario ")
     if user in usuarios:
         print("usuario ja cadastrado, tente outro nome ")
     else:
-        usuarios[user] int(input("informe sua senha, use apenas numeros "))
+        usuarios[user] = input("informe sua senha, use apenas numeros ")
 
 
 def verificacao_de_acesso():
-    login = str(input("informe o seu nome de usuario "))
-    senha = int(input("qual a senha senha?"))
+    login = input("informe o seu nome de usuario ")
+    senha = input("qual a senha senha?")
     if login in usuarios and senha in usuarios[login]:
-    print("usuario permitido")
-elif login not in usuarios or senha not in usuarios[login]:
-    print ("invalido")
+        print("usuario permitido")
+    elif login not in usuarios or senha not in usuarios[login]:
+        print ("invalido")
 
 
 def fim():
     print("fim do sistema")                
+    
+while opcao < 3:
+    if opcao == 0:
+        criar_cadastro()
+
+    elif opcao == 1:
+        verificacao_de_acesso()
+    
+    elif opcao == 2:
+        fim()
+
+    else:
+        print("numero de função invalido")    
