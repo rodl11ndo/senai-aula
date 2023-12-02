96455724e945e9bb90caf19dc084f0fba5e53ca2
rodl11ndo <samuel.m.neto@ba.estudante.senai.br>
2023-12-01 21:13:46 -0300

    commit atividade

---
+++ Exercicio-06.py
@@ -0,0 +1,19 @@
+menu = {
+    'Big mac' : '15.99',
+    'Batata frita' : '7.99',
+    'Prato feito' : '21.99',
+    'Sundae' : '8.99',
+    'Combo junior' : '24.99'
+}
+
+pedido = {
+    
+}
+
+def Exibir_menu():
+    print("Bem vindo ao Mc donalds!\n")
+    print("Menu:\n")
+    print(menu)
+    
+
+print(Exibir_menu())
