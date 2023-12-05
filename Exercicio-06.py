8c8337c3f9a064b6806291f409df409f62e97d01
rodl11ndo <151092224+rodl11ndo@users.noreply.github.com>
2023-12-05 13:28:55 -0300

    Update Exercicio-06.py

--- Exercicio-06.py
+++ Exercicio-06.py
@@ -1,53 +1,96 @@
 menu = {
-    'Big mac' : '15.99',
-    'Batata frita' : '7.99',
-    'Prato feito' : '21.99',
-    'Sundae' : '8.99',
-    'Combo junior' : '24.99'
+    'Big mac': 14.99,
+    'Combo': 22.99,
+    'Batata Frita': 7.99,
+    'Nugget': 8.99,
+    'Combo junio': 24.99
 }
+
 
 pedido = {
-    
+
 }
 
-def Exibir_menu():
-    print("Bem vindo ao Mc donalds!\n")
-    print("Menu:\n")
-    for key,values in menu.items():
-        print(f'{key} : {values}')
-         
 
-print(Exibir_menu())    
-def Adicionar_item():
-    print("Primeiro solicita ao cliente que insira um item do menu")
-    item = input("qual item voce deseja adicionar ao pedido?")
+def exibir_menu():
+    print("\nBem vindo ao Mc donalds!\n")
+    print("Esse é o nosso menu:\n")
+    for item, preco in menu.items():
+        print(f"{item}: R${preco:.2f}")
+
+
+def adicionar_item():
+    item = input("\nqual item você quer adicionar ao pedido?\n")
     if item in menu:
-        pedido['item' : 'values']
-        print(pedido())
-    print("Se o item existir em menu, ela adicionara ao pedido.")
-    print("Se o item existir em pedido, ela adionara a quantidade desejada pelo cliente")
-    print("Caso contrario, ela adicionara o item ao pedido com a quantidade")
-print(Adicionar_item())
+        quantidade = int(input("Digite a quantidade\n"))
+        if item in pedido:
+            pedido[item] += quantidade
+        else:
+            pedido[item] = quantidade
+        print(f"{quantidade} {item} adicionado ao pedido.")
+    else:
+        print("Item não encontrado no menu")
 
 
-def Remove_item():
-    print("Primeiro solicita ao cliente que que informe o item")
-    print("Se o item existir no pedido, ela removerar a quantidade informada pelo cliente")
-    print("Senao imprime que o item não foi encotrado no pedido")
+def remover_item():
+    item = input("Digite o item que você quer remover do pedido\n")
+    if item in pedido and pedido[item] > 0:
+        quantidade = int(input("Digite a quantidade que você quer remover\n"))
+        if quantidade <= pedido[item]:
+            pedido[item] -= quantidade
+            print(f"{quantidade} {item} removido do pedido.")
+        else:
+            print("Quantidade inválida.")
+    else:
+        print("Item não encontrado no pedido.")
 
-def Exibir_pedido():
-    print("exibe todos os itens do pedido")    
 
-def Total_do_pedido():
-    print("ela é usada para calcular o valor total de totos os itens e imprime o total")
+def exibir_pedido():
+    print("PEDIDO:")
+    for item, quantidade in pedido.items():
+        preco_total = quantidade * menu[item]
+        print(f"{item}: {quantidade} - R${preco_total:.2f}")
 
-#Menu principal, é um while que continuamente solicita ao usuarios que informe uma opção
-# 
-# [1] Função exibir menu
-# [2] Função ADD item
-# [3] Remover item
-# [4] Exibir pedido
-# [5] Calcular total
-# [6] Encerra o loop e terminará
-# 
-# Se opção < 6 o programa informa que a opção é invalida
+
+def calcular_total():
+    total = sum(quantidade * menu[item] for item, quantidade in pedido.items())
+    print("TOTAL DO PEDIDO: R${total}\n")
+    print("Qual o metodo de pagamento?\n")
+    print("[1] - CARTÃO")
+    print("[2] - PIX")
+    print("[3] - DINHEIRO")
+    metodo = int(input("Qual a forma de pagamento?"))
+    while True:
+        if metodo == '1':
+           print("CARTÃO APROVADO, PAGAMENTO CONCLUIDO") 
+        elif metodo == '2':
+            print("PIX APROVADO, PAGAMENTO CONCLUIDO")
+        elif metodo == '3':
+            print("PAGAMENTO EM DINHEIRO CONCLUIDO")
+
+while True:
+    print("\nEscolha uma opção:")
+    print("[1] - Exibir Menu")
+    print("[2] - Adicionar Item ao Pedido")
+    print("[3] - Remover Item do Pedido")
+    print("[4] - Exibir Pedido")
+    print("[5] - Calcular Total do Pedido")
+    print("[6] - Encerrar Pedido")
+    
+    opcao = input("opção: ")
+
+    if opcao == '1':
+        exibir_menu()
+    elif opcao == '2':
+        adicionar_item()
+    elif opcao == '3':
+        remover_item()
+    elif opcao == '4':
+        exibir_pedido()
+    elif opcao == '5':
+        calcular_total()
+    elif opcao == '6':
+        print("Pedido encerrado. Obrigado!")
+        break
+    else:
+        print("Opção inválida. Tente novamente.")
