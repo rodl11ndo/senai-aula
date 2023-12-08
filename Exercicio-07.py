8e42b61b27f66d0730261e3b07daec41edad99f5
rodl11ndo <samuel.m.neto@ba.estudante.senai.br>
2023-12-07 21:22:56 -0300

    commit etapa1

---
+++ Exercicio-07.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+dados = {
+    'Mes' : ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+    'Produtos' : ['Coca', 'Coca', 'Coca', 'Coca', 'Coca', 'Coca', 'Pepsi', 'Pepsi', 'Pepsi', 'Pepsi', 'Pepsi', 'Pepsi'],
+    'Quantidade' : [40,70,60,50,80,90,40,70,60,50,80,90]
+    
+}
+
+
+
